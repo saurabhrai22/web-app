22c9ca3ed2938bfacd4cab5ca28829f93615d68b
Adepu, Samran <samran.adepu@accenture.com>
2018-11-16 21:15:04 +0530

    Sprint 1 webapp wireframe

--- webapp/src/app/api.service.ts
+++ webapp/src/app/api.service.ts
@@ -19,18 +19,20 @@ export class ApiService {
   }
   constructor(private http: HttpClient) { }
   //This is for the GET request
-  /* private extractData(res: Response) {
+  private extractData(res: Response) {
     let body = res;
     return body || { };
-  } */
+  }
    
   createHashapi(fcaId): Observable<any> {
     //This is for the GET request
-    /* return this.http.get(this.endpoint + '/convertmd5?fcaid='+fcaId).pipe(
+   return this.http.get(this.endpoint + '/createhash',{headers : new HttpHeaders().set("Access-Control-Allow-Origin","*",).set('Content-Type',  'application/json'), params : new HttpParams().set('fcaid',fcaId)});
+    /* .pipe(
       map(this.extractData)); */
 
     //This is for the POST request
-    return this.http.post(this.endpoint+"/createhash",{'fcaid':fcaId});
+    //return this.http.post(this.endpoint + '/createhash',null,{headers : new HttpHeaders().set("Access-Control-Allow-Origin","*",).set('Content-Type',  'application/json'), params : new HttpParams().set('fcaid',fcaId)});
+    //return this.http.post(this.endpoint+"/createhash",{'fcaid':fcaId});
   }
   generateTicketapi(ticketObj) {
     //This is for the GET request
@@ -38,8 +40,9 @@ export class ApiService {
       map(this.extractData)); */
 
     //This is for the POST request
+    return this.http.get(this.endpoint + '/generatedummyticket',{headers : new HttpHeaders().set("Access-Control-Allow-Origin","*",).set('Content-Type',  'application/json'), params : new HttpParams().set('dataset',ticketObj )});
 
-    return this.http.post(this.endpoint+"/generatedummyticket",null,{params: new HttpParams().set( 'dataset',ticketObj )});
+   // return this.http.post(this.endpoint+"/generatedummyticket",null,{params: new HttpParams().set( 'dataset',ticketObj )});
   }
 //{params: new HttpParams().set( 'fcaid',fcaid )}
   private handleError<T> (operation = 'operation', result?: T) {
